8edeaad6129b310eda7267337397e7e5cc8c8e51
yadnesh ranshevare <yranshevare2005@gmail.com>
2024-12-28 21:13:27 +0530

    done the form of edit profile ad add it into our project

--- src/app/(frontend)/profile/page.tsx
+++ src/app/(frontend)/profile/page.tsx
@@ -1,6 +1,7 @@
 "use client"
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '@/components/header'
+import ProfileForm from '@/components/profileForm'
 
 export default function page() {
 
@@ -10,11 +11,35 @@ export default function page() {
     const [linkTwo,setLinkTwo] = useState("linksTwo")
     // const [image,setImage] = useState("/loading_page_img.jpg") //url of img
     const [image,setImage] = useState("")
+    const [butClick,setButClick] = useState(0)
+    const [opacity,setOpacity] = useState("flex")
+    const [display,setDisplay] = useState("hidden")
+
+    const cancelBut = () => {
+      setDisplay("hidden")
+      setOpacity("flex")
+      setButClick(0)
+    }
+    
+    const openEditProfile = () => {
+      setDisplay("flex")
+      setOpacity("hidden")
+      setButClick(1)
+    }
+   
   return (
     <>
-       <div className='flex flex-col items-center justify-center bg-heroBlue'>
+      <div className={`${display} w-full flex flex-col items-center bg-heroBlue justify-center pt-11`}>
+        <div className='w-full flex justify-end'>
+          <button 
+          onClick={cancelBut}
+          className='h-14 w-14 flex items-center justify-center mr-11 rounded-[50%] opacity-55 bg-red-500 text-white'>X</button>
+        </div>
+        <ProfileForm/>
+      </div>
+       <div className={` flex-col items-center absolute top-0 w-full justify-center bg-heroBlue ${opacity}`}>
             {/* div for hero section */}
-            <div className='w-full h-[70vh] '>
+            <div className= 'w-full h-[70vh]'>
                  <Header/>
                  <div className='w-full h-[500px] flex items-center justify-center  mt-16'>
                     <div className='w-1/2 flex flex-col  justify-around  pl-4'>
@@ -40,7 +65,9 @@ export default function page() {
                             }
                         </div>
                         <div>
-                            <button className='border p-2 rounded-xl hover:bg-[rgba(255,255,255,0.2)] w-full '>edit profile</button>
+                            <button 
+                            onClick={openEditProfile}
+                            className='border p-2 rounded-xl hover:bg-[rgba(255,255,255,0.2)] w-full '>edit profile</button>
                         </div>
 
                     </div>
@@ -69,7 +96,9 @@ export default function page() {
              <div className='w-full shadow-top bg-white text-black flex flex-col items-center'> 
                 <div className='flex justify-between w-[90%]'>
                     <h2 className='p-2 font-black text-2xl text-gray-600'>projects</h2>
-                    <button className='p-2'>Add project</button>
+                    <button 
+                    onClick={() => setButClick(2)}
+                    className='p-2'>Add project</button>
                 </div>
                 <div>
                     
--- src/components/profileForm.jsx
+++ src/components/profileForm.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function ProfileForm() {
+    const [name,setName] = useState("")
+    const [bio,setBio] = useState("")
+    const [linkOne,setLinkOne] = useState("")
+    const [linkTwo, setLinkTwo] = useState("")
+    const [bioCharCount,setBioCharCount] = useState(0)
+    const [bioCounterError,setBioCounterError] = useState("text-gray-400")
+    const [nameError,setNameError] = useState("")
+    const [bioError,setBioError] = useState("")
+
+    useEffect(()=>{
+        setBioCharCount(bio.length)
+        if(bio.length > 250){
+            setBioCounterError("text-red-700")
+        }else{
+            setBioCounterError("text-gray-400")
+        }
+    },[bio])
+
+
+    function validations(){
+        if(name.trim() === "" || bio.trim() === "" ){
+            setNameError("All fields are required")
+            setBioError("All fields are required")
+            return false
+        }
+    }
+
+    function submit(){
+        // console.log(name,bio,linkOne,linkTwo)
+        if(!validations()){
+            return
+        }
+
+
+    }
   return (
-    <div className='flex flex-col items-center justify-around h-[700px] rounded-xl my-8 bg-cardBackground w-1/2 '>
+    <div className='flex flex-col items-center justify-around h-[900px] rounded-xl my-8 bg-cardBackground w-1/2 '>
         <div className='w-[95%] border-b text-center h-[25%] flex  items-center justify-center my-2 pb-3'>
             <div className='w-[100%] flex flex-col items-center justify-center'>
                 <input type="file"  id='inputImage' className='hidden'/>
@@ -11,42 +46,59 @@ export default function ProfileForm() {
                     <label 
                     htmlFor="inputImage" 
                     className='hover:cursor-pointer h-full w-full flex items-center justify-center rounded-[50%]'>
-                        <div className='w-[95%] h-[95%] bg-[rgba(255,255,255,0.2)] rounded-[50%]'>
-                        
+                        <div className='w-[95%] h-[95%] bg-[rgba(255,255,255,0.2)] flex items-center justify-center rounded-[50%]'>
+                                    <p className='font-normal text-sm opacity-65'>set profile image</p>
                         </div>
                     </label>
                 </div>
             </div>
         </div>
         <div className=' flex justify-center flex-col my-2 w-[95%]'>
-            <p className='font-normal text-sm opacity-65'>Your Name*</p>
+            <p className='font-normal text-sm opacity-65'>YOUR NAME*</p>
             <input 
-            type="text" 
+            type="text"
+            value={name}
+            onChange={(e)=> setName(e.target.value)} 
+            onFocus={()=>setNameError("")}
             className='w-[100%] rounded-md font-normal text-sm pl-1 focus:outline-none text-heroBlue py-1'
             placeholder="Name" />
+            <p className='text-xs text-red-700 font-normal h-2'>{nameError}</p>
             <br />
-            <p className='font-normal text-sm opacity-65'>bio*</p>
+            <p className='font-normal text-sm opacity-65'>BIO*</p>
             <textarea 
             name="bio" 
             id="bio"
             placeholder='bio'
             rows="4"
+            value={bio}
+            onChange={(e)=> setBio(e.target.value)}
+            onFocus={()=> setBioError("")}
             className='w-[100%]  rounded-md font-normal text-sm pl-1 focus:outline-none text-heroBlue py-1'
             ></textarea>
+            <div className='flex'>
+                <p className='w-1/2 text-xs text-red-700 font-normal h-2'>{bioError}</p>
+                <p className={`text-end font-normal text-xs w-1/2 mt-2 ${bioCounterError}`}>{bioCharCount}/250</p>
+            </div>
         </div>
         <div className=' flex justify-center flex-col my-2 w-[95%]'>
-            <p className='font-normal text-sm opacity-65'>links</p>
+            <p className='font-normal text-sm opacity-65'>LINKS</p>
             <input 
-            type="text" 
+            type="text"
+            value={linkOne}
+            onChange={(e)=> setLinkOne(e.target.value)} 
             className='w-[100%] rounded-md font-normal text-sm pl-1 focus:outline-none text-heroBlue py-1 mb-2'
             placeholder='link one'/>
             <input 
             type="text" 
+            value={linkTwo}
+            onChange={(e)=> setLinkTwo(e.target.value)}
             className='w-[100%] rounded-md font-normal text-sm pl-1 focus:outline-none text-heroBlue py-1'
             placeholder='link two'/>
         </div>
         <div className=' flex justify-center flex-col my-2 w-[95%]'>
-            <button className='border p-2 rounded-xl hover:bg-[rgba(255,255,255,0.2)] '>save</button>
+            <button 
+            onClick={submit}
+            className='border p-2 rounded-xl hover:bg-[rgba(255,255,255,0.2)] '>save</button>
         </div>
     </div>
   )
